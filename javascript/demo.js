079f1609f46cd44702a92ac950ae8d8a1ff8f1b0
Manish kumar verma <manishverma3922@gmail.com>
2024-12-25 13:17:35 +0530

    Add files via upload

---
+++ javascript/demo.js
@@ -0,0 +1,211 @@
+// console.log("hello world");   // Print "hello world" to the console
+// var x = 5;    // Assign the value 5 to x
+// console.log(x);  // Print the value of x
+// var y = 10;   // Assign the value 10 to y
+// console.log(y);     // Print the value of y
+// console.log(x + y);     // Print the sum of x and y
+
+
+
+// let a = "ram"; // Assign the value "ram" to a
+// console.log("hello manish i am",a,"how is your studies going","\ntype of A variable is",typeof(a));        
+
+
+// let se ham kisi ek variable ko re declare nahi kaar sakte hai
+// jabkivar ke dwara ham kisi variable ko dubara declare kaar sakte hai
+// const ke dwrw variable ko declare karne ke baad ham uss variable ki value ko dubara update nahi kaar sakte hai
+
+
+// var ko ham block ke bahar bhi use kaar sakte hai jabki const aur let o nahi kaar sakte hai
+
+// var amit;
+// console.log(amit);
+// let sonu;
+// console.log(sonu);
+// // const angad;
+// // console.log(angad)
+
+// operators in javascript
+
+// 1. arithmetc operator
+// 2. ASSIGNMENT OPERATOR
+// 3. COMPARISION OPERATOR
+// 4. LOGICAL OPERATOR
+// 5. BITWISE OPERATOR
+// 6. CONDITIONAL OPERATOR
+// 7. TYPE OPERATOR
+// 8. STRING OPERATOR
+// 9. COMMA OPERATOR
+// 10. UNARY OPERATOR
+// 11. RELATIONAL OPERATOR
+// 12. TERNARY OPERATOR
+// 13. VOID OPERATOR
+
+
+
+// NOW LEARNING  OPERATORS IN DETAIL
+
+
+// ARTHEMATIC OPERATORS +, -, **, *, /,% ,++, --,
+
+// console.log(5+2);
+// console.log(5-2);
+// console.log(5**2);
+// console.log(5*2);
+// console.log(6/2);
+// var manish = 5;
+// console.log(++manish);
+// console.log(--manish);
+
+
+// level 2
+
+
+//  if, else if , else statements
+
+// let x = 6;
+// if(x==5){
+//     console.log("x= 5");
+// }
+// else if(x==6){
+//     console.log("x=6");
+// }
+// else{
+//     console.log("condition doesn't exist");
+// }
+
+
+
+
+// loops in javascript
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
